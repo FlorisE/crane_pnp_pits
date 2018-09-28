40d7bf004aaa243e44d53250f28bfcd40620f0e7
Floris Erich <floris.erich@aist.go.jp>
2018-09-28 09:59:15 +0900

    Added an invariant for testing

--- bin/test_picks_and_places.py
+++ bin/test_picks_and_places.py
@@ -2,18 +2,27 @@
 
 from rospit.framework import Runner, TestSuite, TestCase, Sensor
 from rospit.binary import BinaryConditionEvaluator, BinaryCondition, BinaryMeasurement
-from rospit.numeric import BothLimitsEvaluator, BothLimitsCondition, UpperLimitCondition, UpperLimitEvaluator
-from rsj_2017_block_finder.srv import GetBlock, SetThreshold
-from rsj_2017_pick_and_placer.srv import DoPick, DoPickRequest, DoPlace, DoPlaceRequest, Initialize, InitializeRequest
+from rospit.numeric import BothLimitsEvaluator, BothLimitsCondition, \
+                           UpperLimitCondition, UpperLimitEvaluator, \
+                           LowerLimitCondition, LowerLimitEvaluator
+from rospit.ros import ROSTestCase, ROSInvariant, wait_for_service
+from pick_and_placer_msgs.srv import DoPick, DoPickRequest, DoPlace, \
+                                     DoPlaceRequest, Initialize, InitializeRequest
 from geometry_msgs.msg import PoseStamped
 import rospy
 import time
+import logging
+import sys
 
 
-def wait_for_service(service_name):
-    print("Waiting for {} service to become available".format(service_name))
-    rospy.wait_for_service(service_name)
-    return service_name
+# def debug_signal_handler(signal, frame):
+#     import pdb
+#     pdb.set_trace()
+#
+#
+# import signal
+#
+# signal.signal(signal.SIGINT, debug_signal_handler)
 
 
 class PresenceSensor(Sensor):
@@ -59,17 +68,22 @@ class Picks(TestCase):
         block_present_condition = BinaryCondition(True)
         self.preconditions = [(block_present_condition, block_present_evaluator)]
 
-        block_picked_pos_evaluator = BinaryConditionEvaluator(lambda: self.block_sensor.pose.pose.position.z > 0.1)
+        block_picked_pos_evaluator = BinaryConditionEvaluator(
+            lambda: self.block_sensor.pose.pose.position.z > 0.1)
         block_picked_pos_condition = BinaryCondition(True)
         block_picked_moveit_evaluator = BinaryConditionEvaluator(lambda: self.picked)
         block_picked_moveit_condition = BinaryCondition(True)
-        self.postconditions = [(block_picked_pos_condition, block_picked_pos_evaluator), (block_picked_moveit_condition, block_picked_moveit_evaluator)]
+        self.postconditions = [
+            (block_picked_pos_condition, block_picked_pos_evaluator),
+            (block_picked_moveit_condition, block_picked_moveit_evaluator)]
 
     def set_up(self):
         service_name = wait_for_service("/pick_and_placer/initialize")
+        print("Initializing")
         do_initialize = rospy.ServiceProxy(service_name, Initialize)
         initialize_msg = InitializeRequest()
         do_initialize(initialize_msg)
+        print("Initialized")
 
     def run(self):
         service_name = wait_for_service("/pick_and_placer/do_pick")
@@ -85,24 +99,35 @@ class Picks(TestCase):
         print("Finished executing pick")
 
 
-class Places(TestCase):
+class Places(ROSTestCase):
     def __init__(self, depends_on):
-        TestCase.__init__(self, "Places", depends_on=depends_on, wait_for_preconditions=True)
+        ROSTestCase.__init__(self, "Places", depends_on=depends_on, wait_for_preconditions=True)
         self.block_sensor = BlockPoseSensor()
         self.placed = False
-        
+
         block_present_evaluator = BinaryConditionEvaluator(self.block_sensor)
         block_present_condition = BinaryCondition(True)
-        block_picked_evaluator = BinaryConditionEvaluator(lambda: self.block_sensor.pose.pose.position.z > 0.1)
+        block_picked_evaluator = BinaryConditionEvaluator(
+            lambda: self.block_sensor.pose.pose.position.z > 0.1)
         block_picked_condition = BinaryCondition(True)
-        self.preconditions = [(block_present_condition, block_present_evaluator), (block_picked_condition, block_picked_evaluator)]
-        
+        self.preconditions = [
+            (block_present_condition, block_present_evaluator),
+            (block_picked_condition, block_picked_evaluator)]
+
+        inv_condition = LowerLimitCondition(0.1)
+        inv_evaluator = LowerLimitEvaluator(lambda data: data.pose.position.z)
+        self.invariants = [
+            ROSInvariant(inv_condition, inv_evaluator, "/ar_block_finder/pose", PoseStamped)]
+
         block_placed_x_condition = BothLimitsCondition(0.07, 0.13)
         block_placed_y_condition = BothLimitsCondition(-0.23, -0.17)
         block_placed_z_condition = UpperLimitCondition(0.08)
-        block_placed_x_evaluator = BothLimitsEvaluator(lambda: self.block_sensor.pose.pose.position.x)
-        block_placed_y_evaluator = BothLimitsEvaluator(lambda: self.block_sensor.pose.pose.position.y)
-        block_placed_z_evaluator = UpperLimitEvaluator(lambda: self.block_sensor.pose.pose.position.z)
+        block_placed_x_evaluator = BothLimitsEvaluator(
+            lambda: self.block_sensor.pose.pose.position.x)
+        block_placed_y_evaluator = BothLimitsEvaluator(
+            lambda: self.block_sensor.pose.pose.position.y)
+        block_placed_z_evaluator = UpperLimitEvaluator(
+            lambda: self.block_sensor.pose.pose.position.z)
         block_placed_moveit_evaluator = BinaryConditionEvaluator(lambda: self.placed)
         block_placed_moveit_condition = BinaryCondition(True)
         self.postconditions = [
@@ -136,10 +161,15 @@ def get_test_suite():
     test_suite.test_cases.append(places_test_case)
     return test_suite
 
+
 if __name__ == '__main__':
-    #import os
-    #clear = lambda: os.system('clear')
-    #clear()
-    runner = Runner()
+    # # clear the screen
+    # import os
+    # clear = lambda: os.system('clear')
+    # clear()
+    logger = logging.getLogger("test picks and places")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    runner = Runner(logger)
     runner.run_suite(get_test_suite())
-    #raw_input()
+    # raw_input()
